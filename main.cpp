349720832217d39e9ad18d2f77ce18ebae528065
Henryawd345 <henrygoodboy2000@gmail.com>
2025-09-20 18:49:28 -0700

    struct for soccer team established

--- main.cpp
+++ main.cpp
@@ -2,5 +2,31 @@
 #include <string>
 using namespace std;
 
+//Stats of each player
+struct Player{
+    string name;
+    string position;
+    int jerseyNumber;
+
+    int* minutes = nullptr; //inner dynamic array: this will be the minutes played per game
+    int games = 0; // how many games
+
+    ~Player(){
+        delete[] minutes;
+    }
+
+};
+
+struct Team
+{
+    Player *roster = nullptr; //Outer dynamic array: player in the team
+    int count = 0;
+
+    ~Team(){
+        delete[] roster;
+    }
+
+};
+
 
 
